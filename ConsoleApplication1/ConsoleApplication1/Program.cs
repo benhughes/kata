6f819a4586ed7aa63eb448304992380f56e41a54
Ben Hughes <benhuggy@gmail.com>
2013-11-04 13:30:24 +0000

    First crack at solving the first problem. Currently not correct

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//http://projecteuler.net/problems
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList multiples = new ArrayList();
+            multiples.Add(3);
+            multiples.Add(5);
+
+
+            Console.WriteLine(SumOfNumbersThatAreMultiplesOf(10, multiples));
+            Console.WriteLine(SumOfNumbersThatAreMultiplesOf(1000, multiples));
+            ;
+
+            Console.WriteLine();
+            Console.WriteLine("hi there again"); 
+        }
+
+        private static Boolean IsMultipleOf(int num, int multiple)
+        {
+            return num % multiple == 0 && num != 0;
+        }
+
+        private static int SumOfNumbersThatAreMultiplesOf(int lowerThan, ArrayList multiples)
+        {
+            int answer = 0;
+            Boolean multipleOf;
+            foreach (int multiple in multiples)
+            {
+                for (int i = 0; i < lowerThan; i++)
+                {
+                    multipleOf = IsMultipleOf(i, multiple);
+
+                    if (multipleOf)
+                    {
+                        answer += i;
+                    }
+                }
+            }
+            return answer;
+        }
+    }
+
+ 
+}
